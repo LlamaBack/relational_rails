ea1e2a75f8b6f1509c1347f8bf7e46b1d415e429
Jake Kim <imjakekim@gmail.com>
2022-08-25 19:06:46 -0700

    created player index spec

--- spec/features/players/index_spec.rb
+++ spec/features/players/index_spec.rb
@@ -3,14 +3,17 @@ require 'rails_helper'
 RSpec.describe '/players' do
     before :each do
         @lakers = NbaTeam.create!(name: "Los Angeles Lakers", championship_wins: 17, western_conference: true)
-        @clippers = NbaTeam.create!(name: "Los Angeles Clippers", championship_wins: 0, western_conference: true)
+        @lebron = Player.create!(name: "LeBron James", player_number: 6, starting_player: true, nba_team_id:@lakers.id)
+        @anthony = Player.create!(name: "Anthony Davis", player_number: 3, starting_player: true, nba_team_id:@lakers.id)
+        @patrick = Player.create!(name: "Patrick Beverley]", player_number: 21, starting_player: false, nba_team_id:@lakers.id)
+
     end
 
     it 'displays the attributes of given team id' do
-        visit "/nba_teams/#{@lakers.id}"
-        expect(page).to have_content(@lakers.name)
-        expect(page).to have_content(@lakers.championship_wins)
-        expect(page).to have_content(@lakers.western_conference)
-        expect(page).to_not have_content(@clippers.name)
+        visit "/players/#{@lakers.id}"
+        save_and_open_page
+        expect(page).to have_content(@lebron.name)
+        expect(page).to have_content(@anthony.player_number)
+        expect(page).to have_content(@patrick.starting_player)
     end
 end
