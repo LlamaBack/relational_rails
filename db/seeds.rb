a0c0980cf163855f36311f9655395f619b1064b4
Jake Kim <imjakekim@gmail.com>
2022-08-25 18:16:11 -0700

    updated seeds for team table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+lakers = NbaTeam.create!(name: "Los Angeles Lakers", championship_wins: 17, western_conference: true)
+warriors = NbaTeam.create!(name: "Golden State Warriors", championship_wins: 7, western_conference: true)
+clippers = NbaTeam.create!(name: "Los Angeles Clippers", championship_wins: 0, western_conference: true)
+
+celtics = NbaTeam.create!(name: "Boston Celtics", championship_wins: 17, western_conference: false)
+bucks = NbaTeam.create!(name: "Milwaukee Bucks", championship_wins: 2, western_conference: false)
+
