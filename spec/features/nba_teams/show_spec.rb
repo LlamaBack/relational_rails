f1bce46e687d37350fad0f7bd9c1852121f53f1c
Jake Kim <imjakekim@gmail.com>
2022-08-25 18:37:52 -0700

    changed and specified tests for /nba_teams

--- spec/features/nba_teams/show_spec.rb
+++ spec/features/nba_teams/show_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe '/nba_teams page' do
+RSpec.describe '/nba_teams/:id page' do
     before :each do
         @lakers = NbaTeam.create!(name: "Los Angeles Lakers", championship_wins: 17, western_conference: true)
         @clippers = NbaTeam.create!(name: "Los Angeles Clippers", championship_wins: 0, western_conference: true)
     end
 
-    it 'displays all nba teams' do
-        visit "/nba_teams"
+    it 'displays the attributes of given team id' do
+        visit "/nba_teams/#{@lakers.id}"
         expect(page).to have_content(@lakers.name)
-        expect(page).to have_content(@clippers.name)
+        expect(page).to have_content(@lakers.championship_wins)
+        expect(page).to have_content(@lakers.western_conference)
+        expect(page).to_not have_content(@clippers.name)
     end
 end
