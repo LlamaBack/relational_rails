6c0f5c65627a2d1230683ce211a71809293043b4
Jake Kim <imjakekim@gmail.com>
2022-08-25 18:50:57 -0700

    destroyed teams table via migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_035916) do
+ActiveRecord::Schema.define(version: 2022_08_26_014506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,14 +31,6 @@ ActiveRecord::Schema.define(version: 2022_08_25_035916) do
     t.datetime "updated_at", null: false
     t.bigint "nba_team_id"
     t.index ["nba_team_id"], name: "index_players_on_nba_team_id"
-  end
-
-  create_table "teams", force: :cascade do |t|
-    t.string "name"
-    t.integer "championship_wins"
-    t.boolean "western_conference"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "players", "nba_teams"
