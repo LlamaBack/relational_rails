8faa5687edf3fee90de5413c98fa24c813d78ef2
Jake Kim <imjakekim@gmail.com>
2022-08-27 18:02:28 -0700

    refactored player index page

--- spec/features/players/index_spec.rb
+++ spec/features/players/index_spec.rb
@@ -7,13 +7,17 @@ RSpec.describe '/players' do
         @anthony = Player.create!(name: "Anthony Davis", player_number: 3, starting_player: true, nba_team_id:@lakers.id)
         @patrick = Player.create!(name: "Patrick Beverley", player_number: 21, starting_player: false, nba_team_id:@lakers.id)
 
+        @clippers = NbaTeam.create!(name: "Los Angeles Clippers", championship_wins: 0, western_conference: true)
+        @paul = Player.create!(name:"Paul George", player_number: 13, starting_player: true,nba_team_id:@clippers.id)
+
     end
 
     it 'displays the attributes of given team id' do
-        visit "/players/#{@lakers.id}"
+        visit "/players"
         save_and_open_page
         expect(page).to have_content(@lebron.name)
         expect(page).to have_content(@anthony.player_number)
         expect(page).to have_content("Is not a starting player.")
+        expect(page).to have_content(@paul.player_number)
     end
 end
